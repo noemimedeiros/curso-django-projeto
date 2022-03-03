e7ff948e9a5ea764c7f28a0a0b40ddd01f850dd4
Noemi <87270671+noemimedeiros@users.noreply.github.com>
2022-03-03 11:24:45 -0300

    Feito cabeçalho da página

--- recipes/views.py
+++ recipes/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'recipes/home.html', context={
+    return render(request, 'coffeeshop/pages/home.html', context={
         'name': 'Noemi Medeiros',
     })
 

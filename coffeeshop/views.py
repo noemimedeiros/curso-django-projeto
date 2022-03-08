5818fb35da261bc60878e51aba8c09725b0ca073
Noemi <87270671+noemimedeiros@users.noreply.github.com>
2022-03-08 14:02:50 -0300

    Arquivo style criado

--- coffeeshop/views.py
+++ coffeeshop/views.py
@@ -2,7 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'coffeeshop/pages/home.html', context={
-        'name': 'Noemi Medeiros',
-    })
+    return render(request, 'coffeeshop/pages/home.html')
 

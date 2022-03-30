de3abc5e64504a91c3acd1fc0c2847d247b7fb89
Noemi <87270671+noemimedeiros@users.noreply.github.com>
2022-03-30 16:47:49 -0300

    Correção de exibição de receitas e Títulos

--- coffeeshop/views.py
+++ coffeeshop/views.py
@@ -1,26 +1,33 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_list_or_404, get_object_or_404
+from django.http import Http404
 
 from utils.coffeeshop.factory import make_recipe
 from .models import Recipe
 
 def home(request):
-
     recipes = Recipe.objects.filter(is_published=True).order_by('-id')
+
     return render(request, 'coffeeshop/pages/home.html', context={
         'recipes': recipes,
     })
 
 def category(request, category_id):
-
-    recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            category__id=category_id, 
+            is_published=True
+        ).order_by('-id'))
+    
     return render(request, 'coffeeshop/pages/category.html', context={
         'recipes': recipes,
+        'title': f'{recipes[0].category.name} - Category | '
     })
 
 
 def recipes(request, id):
+    recipe = get_object_or_404(Recipe, pk=id, is_published=True)
+
     return render(request, 'coffeeshop/pages/recipes-view.html', context={
-        'recipe': make_recipe(),
+        'recipe': recipe,
         'is_detail_page': True,
     })

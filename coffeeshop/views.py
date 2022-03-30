6c23469ec8f30fdaff73380a1ff30a43ba53e1fe
Noemi <87270671+noemimedeiros@users.noreply.github.com>
2022-03-30 14:37:09 -0300

    Corrigindo exibição do autor

--- coffeeshop/views.py
+++ coffeeshop/views.py
@@ -2,11 +2,22 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from utils.coffeeshop.factory import make_recipe
+from .models import Recipe
 
 def home(request):
+
+    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
     return render(request, 'coffeeshop/pages/home.html', context={
-        'recipes': [make_recipe() for _ in range(9)],
+        'recipes': recipes,
     })
+
+def category(request, category_id):
+
+    recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
+    return render(request, 'coffeeshop/pages/category.html', context={
+        'recipes': recipes,
+    })
+
 
 def recipes(request, id):
     return render(request, 'coffeeshop/pages/recipes-view.html', context={
